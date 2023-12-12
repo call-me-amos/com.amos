9fb54fc31e179f38bb0c0c783e9b150ba9f5c430
amos.tong@corp.to8to.com <amos.tong@corp.to8to.com>
2023-12-12 21:50:29 +0800

    xx

--- src/main/java/com/test/test/Test2.java
+++ src/main/java/com/test/test/Test2.java
@@ -1,20 +1,48 @@
 package com.test.test;
 
-import io.netty.util.internal.ThrowableUtil;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test2 {
-    public static void main(String[] args) throws ParseException {
+    private static final Pattern PATTERN_HOUSE = Pattern.compile("毛坯");
+    private static final Pattern PATTERN_INIT = Pattern.compile("农村");
 
-        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        String hourAndMinutes = "2023-08-30 18:00";
-        System.out.println("====="+sdf2.parse(hourAndMinutes).getTime()/1000L);
+    public static void main(String[] args) throws ParseException {
+        Date date = parse("2023-11-16 "+"09:00:00", "yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("==" + date.getTime());
+
+        String f = format(new Date(), "yyyy-MM-dd");
+        System.out.println("f="+f);
 
     }
+
+    public static Date parse(String text, String format) {
+        try {
+            if (format != null && !"".equals(format) && text != null) {
+                SimpleDateFormat formatter = new SimpleDateFormat(format, Locale.US);
+                return formatter.parse(text);
+            }
+        } catch (Exception var4) {
+        }
+
+        return null;
+    }
+    public static String format(Date date, String format) {
+        try {
+            if (format != null && !"".equals(format) && date != null) {
+                SimpleDateFormat formatter = new SimpleDateFormat(format);
+                return formatter.format(date);
+            }
+        } catch (Exception var3) {
+        }
+
+        return null;
+    }
+
+
 }
-// 2023-08-30 18:00
-// 2023-07-31 18:00:00  yyyy-MM-dd HH:dd
-// 2023-08-30 18:00:00  yyyy-MM-dd HH:mm
